d76eaf2aae032eb45a9e18a2354f63657d598eea
zeus8881 <vn1415843@gmail.com>
2025-10-16 13:27:05 +0300

    11

--- userservices/src/main/java/microservices/userservices/UserservicesApplication.java
+++ userservices/src/main/java/microservices/userservices/UserservicesApplication.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import java.time.Duration;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserservicesApplication {
 
     public static void main(String[] args) {
